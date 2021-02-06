dd48d2c750e4b9dc080f05d75a2de2db8ae5fbd1
Ruben Villalvazo <vim.ruben@hotmail.com>
2021-02-06 01:59:48 -0300

    Movimiento del jugador en general

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ light_grey = (200, 200, 200)
 
 ball_speed_x = 7
 ball_speed_y = 7
+player_speed = 0
 
 def ball_animation():
     global ball_speed_x, ball_speed_y
@@ -35,14 +36,32 @@ def ball_animation():
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1
 
+def player_animation():
+    player.y += player_speed
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height 
+
 while True:
     #Imput control
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.QUIT()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:  
+                player_speed += 7
+            if event.key == pygame.K_UP:
+                player_speed -= 7
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:  
+                player_speed -= 7
+            if event.key == pygame.K_UP:
+                player_speed += 7   
 
     ball_animation()
+    player_animation()
 
     #visual
     screen.fill(bg_color)
